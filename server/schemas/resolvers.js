6ccd55a2d1756c5cdeaafb05108a2435b2a9c071
Jeremy <jdskinne@gmail.com>
2023-01-29 22:24:09 -0600

    added boilerplate typedefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+
+    },
+};
+
+module.exports = resolvers;
